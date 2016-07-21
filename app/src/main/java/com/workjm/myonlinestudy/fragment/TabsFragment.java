db48c151e2623472100ac5e41b7cb262c363d3e6
Liangjm <junming.liang@tinno.com>
2016-07-21 13:56:57 +0800

    tab selected

--- app/src/main/java/com/workjm/myonlinestudy/fragment/TabsFragment.java
+++ app/src/main/java/com/workjm/myonlinestudy/fragment/TabsFragment.java
@@ -57,7 +57,7 @@ public class TabsFragment extends BaseFragment{
         mTabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                viewPager.setCurrentItem(tab.getPosition());
             }
 
             @Override
